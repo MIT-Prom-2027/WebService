660b002e297598394455f33facdc227e1c731275
fandresena37 <fandresenarazafindratina@gmail.com>
2025-09-03 08:52:03 +0300

    refactor: remove obsolete InputModel and OutputModel; comment out Sexe property in PreinscriptionResponse

--- Dtos/PreinscriptionResponse.cs
+++ Dtos/PreinscriptionResponse.cs
@@ -5,7 +5,7 @@ namespace UnivManager.Dtos
         public string? Nom_prenom { get; set; }
         public DateTime Date_naissance { get; set; }
         public string? Lieu_naissance { get; set; }
-        public string? Sexe { get; set; }
+        // public string? Sexe { get; set; }
         public string? Mention { get; set; }
         public string? Option { get; set; }
         public string? Num_bacc { get; set; }
